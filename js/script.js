5ba777eed5b26f3efaa8a291920f139daca51add
Ash <hurairah.asghar2003@gmail.com>
2019-11-26 23:05:34 +0000

    debugging

--- js/script.js
+++ js/script.js
@@ -5,7 +5,23 @@
 //GLOBAL VARIABLES
 /* global $ */
 
-
+var userChoice= "";
+var compChoice= "";
+var winner= "";
+let randomNumber= 0;
+$("#shoot").click(function() {
+    let randomNumber=Math.random();
+    let userChoice= $("#input").val();
+    $("#result").text(userChoice);
+    if (randomNumber <= 0.33) {
+        debugger;
+        console.log("paper");
+    } else if (randomNumber >= 0.34 && randomNumber <= 0.66) {
+        console.log("rock");
+    } else {
+        console.log("scissors");
+    }
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
